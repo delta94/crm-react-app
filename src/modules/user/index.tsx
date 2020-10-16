f2c5676a5268ecb9cba31cf852355b41ce29539b
Dilshod Turobov <deebov@yandex.com>
2020-10-16 17:44:40 +0500

    add logout

---
+++ src/modules/user/index.tsx
@@ -0,0 +1,24 @@
+import { Box, Button } from "@chakra-ui/core";
+import SectionHeading from "components/SectionHeading";
+import { axios } from "helpers/api";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { FLUSH } from "store/actionTypes";
+
+const User = () => {
+  const dispatch = useDispatch();
+  const logout = () => {
+    dispatch({ type: FLUSH });
+    delete axios.defaults.headers.common["Authorization"];
+  };
+  return (
+    <Box>
+      <SectionHeading mb={10}>User</SectionHeading>
+      <Button colorScheme="blue" onClick={logout}>
+        Log out
+      </Button>
+    </Box>
+  );
+};
+
+export default User;
